b30deb5c997f90dcf7e31ad9565aa7825e9a3cf9
DF-OST <diego.fantino@ost.ch>
2023-08-09 09:46:09 +0200

    m

--- calorieCounter/calorieCounter/settings.py
+++ calorieCounter/calorieCounter/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-jncaqbu*hq7a)u0yjt5%#yw@@73p91vd0j853hid$m(ghffij6
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [ '127.0.0.1:8000',
+ALLOWED_HOSTS = [ '127.0.0.1',
                  'dfost.pythonanywhere.com',]
 
 
