1c9cf539ad9a9cb81e9ba8a82bbcc0282329271a
DF-OST <diego.fantino@ost.ch>
2023-08-09 09:39:51 +0200

    mini cahnges

--- calorieCounter/counter/urls.py
+++ calorieCounter/counter/urls.py
@@ -4,8 +4,8 @@ from . import views
 urlpatterns = [
     path('', views.meal_list, name='meal_list'),
     path('go/', views.go, name='go'),
-    path('go/history/', views.meal_list_all, name='meal_list_all'),
-    path('go/history/<str:date>', views.meal_history_list, name='meal_history_list'),
+    path('history/', views.meal_list_all, name='meal_list_all'),
+    path('history/<str:date>', views.meal_history_list, name='meal_history_list'),
     path('meal/<int:pk>/', views.meal_detail, name='meal_detail'),
     path('meal/new/', views.meal_new, name='meal_new'),
     path('meal/<int:pk>/edit/', views.meal_edit, name='meal_edit'),
